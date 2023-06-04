af052fc0e81f6458850d35b18207389eb683967f
Ismar Kovačević <116498714+Duhkha@users.noreply.github.com>
2023-06-04 15:28:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Get the necessary elements
+
 const firstScreen = document.getElementById('first-screen');
 const secondScreen = document.getElementById('second-screen');
 const goToSecondScreenButton = document.getElementById('go-to-second-screen');
 const goToFirstScreenButton = document.getElementById('go-to-first-screen');
 
-// Add a click event listener to the "Go to Second Screen" button
+
 goToSecondScreenButton.addEventListener('click', function() {
   // Hide the first screen
   firstScreen.style.display = 'none';
@@ -13,7 +13,7 @@ goToSecondScreenButton.addEventListener('click', function() {
   secondScreen.style.display = 'block';
 });
 
-// Add a click event listener to the "Go to First Screen" button
+
 goToFirstScreenButton.addEventListener('click', function() {
   // Hide the second screen
   secondScreen.style.display = 'none';
